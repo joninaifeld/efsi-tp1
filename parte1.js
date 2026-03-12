494372dad2259fcb410c58feb612126cb07eb853
Android Studio <devandroid@ort>
2026-03-12 08:50:40 -0300

    partes 2, 3, 4 y 5

--- parte1.js
+++ parte1.js
@@ -8,6 +8,7 @@ function formatearNombre(nombre) {
 }
 console.log(`formatearNombre("asdwasdwasd") -> ${formatearNombre("asdwasdwasd")}`)
 
+//2
 function contarLetras(texto){
     let count = 0
     for(let char of texto){
@@ -16,3 +17,19 @@ function contarLetras(texto){
     return count
 }
 console.log(`contarLetras("123 123") -> ${contarLetras("123 123")}`)
+
+//3
+function maximo(a, b, c){
+    let max = a
+    if (b > max) max = b
+    if (c > max) max = c
+    return max
+}
+
+console.log("maximo(a, b, c) -> " + maximo(3, 5, 6))
+
+function validarPassword(password){
+    return password.length >= 8 && Array.from(password).some(char => !isNaN(char))
+}
+
+console.log(`validarPassword("jashasjsaj9") -> ${validarPassword("jashasjsaj9")}`)
